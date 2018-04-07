51186fa54710d79e927645d9157d2ab5fdf237c9
Marek <fotelpl@gmail.com>
2018-04-07 16:26:50 +0200

    Updates
    Moved strings to progmem to free RAM
    Added buttons (see readme)
    Slightly formatted code
    Reading config directly from file (prevents corruption)
    currentState is properly read (related to method of reading)

--- NidayandHelper.cpp
+++ NidayandHelper.cpp
@@ -1,35 +1,43 @@
 #include "Arduino.h"
 #include "NidayandHelper.h"
 
-NidayandHelper::NidayandHelper(){
+NidayandHelper::NidayandHelper() {
   this->_configfile = "/config.json";
   this->_mqttclientid = ("ESPClient-" + String(ESP.getChipId()));
-
 }
 
-boolean NidayandHelper::loadconfig(){
+boolean NidayandHelper::loadconfig() {
   File configFile = SPIFFS.open(this->_configfile, "r");
   if (!configFile) {
-    Serial.println("Failed to open config file");
+    Serial.println(F("Failed to open config file"));
     return false;
   }
 
   size_t size = configFile.size();
   if (size > 1024) {
-    Serial.println("Config file size is too large");
+    Serial.println(F("Config file size is too large"));
     return false;
   }
 
   // Allocate a buffer to store contents of the file.
-  std::unique_ptr<char[]> buf(new char[size]);
+  //std::unique_ptr<char[]> buf(new char[size]);
 
   // We don't use String here because ArduinoJson library requires the input
   // buffer to be mutable. If you don't use ArduinoJson, you may as well
   // use configFile.readString instead.
-  configFile.readBytes(buf.get(), size);
+  //configFile.readBytes(buf.get(), size);
 
-  StaticJsonBuffer<200> jsonBuffer;
-  this->_config = jsonBuffer.parseObject(buf.get());
+  //No more memory leaks
+  DynamicJsonBuffer jsonBuffer(300);
+
+  //Reading from buffer breaks first key
+  //this->_config = jsonBuffer.parseObject(buf.get());
+
+  //Reading directly from file DOES NOT cause currentPosition to break
+  this->_config = jsonBuffer.parseObject(configFile);
+
+  //Avoid leaving opened files
+  configFile.close();
 
   if (!this->_config.success()) {
     Serial.println("Failed to parse config file");
@@ -38,11 +46,11 @@ boolean NidayandHelper::loadconfig(){
   return true;
 }
 
-JsonVariant NidayandHelper::getconfig(){
+JsonVariant NidayandHelper::getconfig() {
   return this->_config;
 }
 
-boolean NidayandHelper::saveconfig(JsonVariant json){
+boolean NidayandHelper::saveconfig(JsonVariant json) {
   File configFile = SPIFFS.open(this->_configfile, "w");
   if (!configFile) {
     Serial.println("Failed to open config file for writing");
@@ -50,7 +58,8 @@ boolean NidayandHelper::saveconfig(JsonVariant json){
   }
 
   json.printTo(configFile);
-
+  configFile.flush(); //Making sure it's saved
+  
   Serial.println("Saved JSON to SPIFFS");
   json.printTo(Serial);
   Serial.println();
@@ -61,21 +70,23 @@ String NidayandHelper::mqtt_gettopic(String type) {
   return "/raw/esp8266/" + String(ESP.getChipId()) + "/" + type;
 }
 
-
-void NidayandHelper::mqtt_reconnect(PubSubClient& psclient){
+void NidayandHelper::mqtt_reconnect(PubSubClient& psclient) {
   return mqtt_reconnect(psclient, String(NULL), String(NULL));
 }
-void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, std::list<const char*> topics){
+
+void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, std::list<const char*> topics) {
   return mqtt_reconnect(psclient, String(NULL), String(NULL), topics);
 }
-void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, String uid, String pwd){
+
+void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, String uid, String pwd) {
   std::list<const char*> mylist;
   return mqtt_reconnect(psclient, uid, pwd, mylist);
 }
-void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, String uid, String pwd, std::list<const char*> topics){
+
+void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, String uid, String pwd, std::list<const char*> topics) {
   // Loop until we're reconnected
   boolean mqttLogon = false;
-  if (uid!=NULL and pwd != NULL){
+  if (uid != NULL and pwd != NULL) {
     mqttLogon = true;
   }
   while (!psclient.connected()) {
@@ -85,13 +96,13 @@ void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, String uid, String p
       Serial.println("connected");
 
       //Send register MQTT message with JSON of chipid and ip-address
-      this->mqtt_publish(psclient, "/raw/esp8266/register", "{ \"id\": \"" + String(ESP.getChipId()) + "\", \"ip\":\"" + WiFi.localIP().toString() +"\"}");
+      this->mqtt_publish(psclient, "/raw/esp8266/register", "{ \"id\": \"" + String(ESP.getChipId()) + "\", \"ip\":\"" + WiFi.localIP().toString() + "\"}");
 
       //Setup subscription
-      if (!topics.empty()){
-        for (const char* t : topics){
-           psclient.subscribe(t);
-           Serial.println("Subscribed to "+String(t));
+      if (!topics.empty()) {
+        for (const char* t : topics) {
+          psclient.subscribe(t);
+          Serial.println("Subscribed to " + String(t));
         }
       }
 
@@ -104,22 +115,22 @@ void NidayandHelper::mqtt_reconnect(PubSubClient& psclient, String uid, String p
       delay(5000);
     }
   }
-  if (psclient.connected()){
+  if (psclient.connected()) {
     psclient.loop();
   }
 }
 
-void NidayandHelper::mqtt_publish(PubSubClient& psclient, String topic, String payload){
-  Serial.println("Trying to send msg..."+topic+":"+payload);
+void NidayandHelper::mqtt_publish(PubSubClient& psclient, String topic, String payload) {
+  Serial.println("Trying to send msg..." + topic + ":" + payload);
   //Send status to MQTT bus if connected
   if (psclient.connected()) {
     psclient.publish(topic.c_str(), payload.c_str());
   } else {
-    Serial.println("PubSub client is not connected...");
+    Serial.println(F("PubSub client is not connected..."));
   }
 }
 
-void NidayandHelper::resetsettings(WiFiManager& wifim){
+void NidayandHelper::resetsettings(WiFiManager& wifim) {
   SPIFFS.format();
   wifim.resetSettings();
 }
--- motor_on_a_roller_blind-ws.ino
+++ motor_on_a_roller_blind-ws.ino
@@ -1,27 +1,32 @@
-#include <Stepper_28BYJ_48.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <PubSubClient.h>
-#include <WiFiUdp.h>
-#include <WiFiManager.h>
 #include <ArduinoJson.h>
-#include "FS.h"
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <WebSocketsServer.h>
 #include <ArduinoOTA.h>
-#include "NidayandHelper.h"
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Stepper_28BYJ_48.h>
+#include <WebSocketsServer.h>
+#include <WiFiClient.h>
+#include <WiFiManager.h>
+#include <WiFiUdp.h>
+#include "FS.h"
 #include "index_html.h"
+#include "NidayandHelper.h"
 
 //--------------- CHANGE PARAMETERS ------------------
 //Configure Default Settings for Access Point logon
 String APid = "BlindsConnectAP";    //Name of access point
 String APpw = "nidayand";           //Hardcoded password for access point
 
+//Set up buttons
+const uint8_t btnup = D5; //Up button
+const uint8_t btndn = D6; //Down button
+const uint8_t btnres = D7; //Reset button
+
 //----------------------------------------------------
 
 // Version number for checking if there are new code releases and notifying the user
-String version = "1.3.1";
+String version = "1.3.3";
 
 NidayandHelper helper = NidayandHelper();
 
@@ -56,14 +61,18 @@ ESP8266WebServer server(80);              // TCP server at port 80 will respond 
 WebSocketsServer webSocket = WebSocketsServer(81);  // WebSockets will respond on port 81
 
 bool loadConfig() {
-  if (!helper.loadconfig()){
+  if (!helper.loadconfig())
     return false;
-  }
+
   JsonVariant json = helper.getconfig();
 
+  //Useful if you need to see why confing is read incorrectly
+  json.printTo(Serial);
+
   //Store variables locally
-  currentPosition = long(json["currentPosition"]);
-  maxPosition = long(json["maxPosition"]);
+  currentPosition = json["currentPosition"].as<long>();
+  maxPosition = json["maxPosition"].as<long>();
+
   strcpy(config_name, json["config_name"]);
   strcpy(mqtt_server, json["mqtt_server"]);
   strcpy(mqtt_port, json["mqtt_port"]);
@@ -79,7 +88,7 @@ bool loadConfig() {
    on SPIFFS
 */
 bool saveConfig() {
-  StaticJsonBuffer<200> jsonBuffer;
+  DynamicJsonBuffer jsonBuffer(300);
   JsonObject& json = jsonBuffer.createObject();
   json["currentPosition"] = currentPosition;
   json["maxPosition"] = maxPosition;
@@ -107,7 +116,7 @@ void sendmsg(String topic, String payload) {
 
 /****************************************************************************************
 */
-void processMsg(String res, uint8_t clientnum){
+void processMsg(String res, uint8_t clientnum) {
   /*
      Check if calibration is running and if stop is received. Store the location
   */
@@ -116,50 +125,43 @@ void processMsg(String res, uint8_t clientnum){
     saveItNow = true;
   }
 
-  /*
-     Below are actions based on inbound MQTT payload
-  */
+  //Below are actions based on inbound MQTT payload
   if (res == "(start)") {
-    /*
-       Store the current position as the start position
-    */
+
+    //Store the current position as the start position
     currentPosition = 0;
     path = 0;
     saveItNow = true;
     action = "manual";
   } else if (res == "(max)") {
-    /*
-       Store the max position of a closed blind
-    */
+
+    //Store the max position of a closed blind
     maxPosition = currentPosition;
     path = 0;
     saveItNow = true;
     action = "manual";
   } else if (res == "(0)") {
-    /*
-       Stop
-    */
+
+    //Stop
     path = 0;
     saveItNow = true;
     action = "manual";
   } else if (res == "(1)") {
-    /*
-       Move down without limit to max position
-    */
+
+    //Move down without limit to max position
     path = 1;
     action = "manual";
   } else if (res == "(-1)") {
-    /*
-       Move up without limit to top position
-    */
+
+    //Move up without limit to top position
     path = -1;
     action = "manual";
   } else if (res == "(update)") {
     //Send position details to client
-    int set = (setPos * 100)/maxPosition;
-    int pos = (currentPosition * 100)/maxPosition;
-    sendmsg(outputTopic, "{ \"set\":"+String(set)+", \"position\":"+String(pos)+" }");
-    webSocket.sendTXT(clientnum, "{ \"set\":"+String(set)+", \"position\":"+String(pos)+" }");
+    int set = (setPos * 100) / maxPosition;
+    int pos = (currentPosition * 100) / maxPosition;
+    sendmsg(outputTopic, "{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
+    webSocket.sendTXT(clientnum, "{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
   } else if (res == "(ping)") {
     //Do nothing
   } else {
@@ -172,31 +174,31 @@ void processMsg(String res, uint8_t clientnum){
     setPos = path; //Copy path for responding to updates
     action = "auto";
 
-    int set = (setPos * 100)/maxPosition;
-    int pos = (currentPosition * 100)/maxPosition;
+    int set = (setPos * 100) / maxPosition;
+    int pos = (currentPosition * 100) / maxPosition;
 
     //Send the instruction to all connected devices
-    sendmsg(outputTopic, "{ \"set\":"+String(set)+", \"position\":"+String(pos)+" }");
-    webSocket.broadcastTXT("{ \"set\":"+String(set)+", \"position\":"+String(pos)+" }");
+    sendmsg(outputTopic, "{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
+    webSocket.broadcastTXT("{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
   }
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-    switch(type) {
-        case WStype_TEXT:
-            Serial.printf("[%u] get Text: %s\n", num, payload);
+  switch (type) {
+    case WStype_TEXT:
+      Serial.printf("[%u] get Text: %s\n", num, payload);
 
-            String res = (char*)payload;
+      String res = (char*)payload;
 
-            //Send to common MQTT and websocket function
-            processMsg(res, num);
-            break;
-    }
+      //Send to common MQTT and websocket function
+      processMsg(res, num);
+      break;
+  }
 }
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+  Serial.print(F("Message arrived ["));
   Serial.print(topic);
-  Serial.print("] ");
+  Serial.print(F("] "));
   String res = "";
   for (int i = 0; i < length; i++) {
     res += String((char) payload[i]);
@@ -213,6 +215,7 @@ void stopPowerToCoils() {
   digitalWrite(D2, LOW);
   digitalWrite(D3, LOW);
   digitalWrite(D4, LOW);
+  Serial.println(F("Motor stopped"));
 }
 
 /*
@@ -225,16 +228,16 @@ void saveConfigCallback () {
 void handleRoot() {
   server.send(200, "text/html", INDEX_HTML);
 }
-void handleNotFound(){
+void handleNotFound() {
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
+  for (uint8_t i = 0; i < server.args(); i++) {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
@@ -244,7 +247,11 @@ void setup(void)
 {
   Serial.begin(115200);
   delay(100);
-  Serial.print("Starting now\n");
+  Serial.print(F("Starting now\n"));
+
+  pinMode(btnup, INPUT_PULLUP);
+  pinMode(btndn, INPUT_PULLUP);
+  pinMode(btnres, INPUT_PULLUP);
 
   //Reset the action
   action = "";
@@ -267,7 +274,7 @@ void setup(void)
   WiFiManagerParameter custom_text2("<script>t = document.createElement('div');t2 = document.createElement('input');t2.setAttribute('type', 'checkbox');t2.setAttribute('id', 'tmpcheck');t2.setAttribute('style', 'width:10%');t2.setAttribute('onclick', \"if(document.getElementById('Rotation').value == 'false'){document.getElementById('Rotation').value = 'true'} else {document.getElementById('Rotation').value = 'false'}\");t3 = document.createElement('label');tn = document.createTextNode('Clockwise rotation');t3.appendChild(t2);t3.appendChild(tn);t.appendChild(t3);document.getElementById('Rotation').style.display='none';document.getElementById(\"Rotation\").parentNode.insertBefore(t, document.getElementById(\"Rotation\"));</script>");
   //Setup WIFI Manager
   WiFiManager wifiManager;
-
+  
   //reset settings - for testing
   //clean FS, for testing
   //helper.resetsettings(wifiManager);
@@ -287,7 +294,7 @@ void setup(void)
 
   //Load config upon start
   if (!SPIFFS.begin()) {
-    Serial.println("Failed to mount file system");
+    Serial.println(F("Failed to mount file system"));
     return;
   }
 
@@ -315,25 +322,25 @@ void setup(void)
   */
   loadDataSuccess = loadConfig();
   if (!loadDataSuccess) {
+    Serial.println(F("Unable to load saved data"));
     currentPosition = 0;
     maxPosition = 2000000;
   }
-
   /*
     Setup multi DNS (Bonjour)
-    */
+  */
   if (MDNS.begin(config_name)) {
-    Serial.println("MDNS responder started");
+    Serial.println(F("MDNS responder started"));
     MDNS.addService("http", "tcp", 80);
     MDNS.addService("ws", "tcp", 81);
 
   } else {
-    Serial.println("Error setting up MDNS responder!");
-    while(1) {
+    Serial.println(F("Error setting up MDNS responder!"));
+    while (1) {
       delay(1000);
     }
   }
-  Serial.print("Connect to http://"+String(config_name)+".local or http://");
+  Serial.print("Connect to http://" + String(config_name) + ".local or http://");
   Serial.println(WiFi.localIP());
 
   //Start HTTP server
@@ -348,26 +355,26 @@ void setup(void)
   /* Setup connection for MQTT and for subscribed
     messages IF a server address has been entered
   */
-  if (String(mqtt_server) != ""){
-    Serial.println("Registering MQTT server");
+  if (String(mqtt_server) != "") {
+    Serial.println(F("Registering MQTT server"));
     psclient.setServer(mqtt_server, String(mqtt_port).toInt());
     psclient.setCallback(mqttCallback);
 
   } else {
     mqttActive = false;
-    Serial.println("NOTE: No MQTT server address has been registered. Only using websockets");
+    Serial.println(F("NOTE: No MQTT server address has been registered. Only using websockets"));
   }
 
-  /* Set rotation direction of the blinds */
-  if (String(config_rotation) == "false"){
+  //Set rotation direction of the blinds
+  if (String(config_rotation) == "false")
     ccw = true;
-  } else {
+  else
     ccw = false;
-  }
+
 
   //Update webpage
-  INDEX_HTML.replace("{VERSION}","V"+version);
-  INDEX_HTML.replace("{NAME}",String(config_name));
+  INDEX_HTML.replace("{VERSION}", "V" + version);
+  INDEX_HTML.replace("{NAME}", String(config_name));
 
 
   //Setup OTA
@@ -379,21 +386,21 @@ void setup(void)
     ArduinoOTA.setHostname(config_name);
 
     ArduinoOTA.onStart([]() {
-      Serial.println("Start");
+      Serial.println(F("Start"));
     });
     ArduinoOTA.onEnd([]() {
-      Serial.println("\nEnd");
+      Serial.println(F("\nEnd"));
     });
     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
       Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
     });
     ArduinoOTA.onError([](ota_error_t error) {
       Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+      if (error == OTA_AUTH_ERROR) Serial.println(F("Auth Failed"));
+      else if (error == OTA_BEGIN_ERROR) Serial.println(F("Begin Failed"));
+      else if (error == OTA_CONNECT_ERROR) Serial.println(F("Connect Failed"));
+      else if (error == OTA_RECEIVE_ERROR) Serial.println(F("Receive Failed"));
+      else if (error == OTA_END_ERROR) Serial.println(F("End Failed"));
     });
     ArduinoOTA.begin();
   }
@@ -407,20 +414,63 @@ void loop(void)
   //Websocket listner
   webSocket.loop();
 
-  /**
-    Serving the webpage
-  */
+  //Serving the webpage
   server.handleClient();
 
   //MQTT client
-  if (mqttActive){
+  if (mqttActive)
     helper.mqtt_reconnect(psclient, mqtt_uid, mqtt_pwd, { inputTopic.c_str() });
+
+  if (digitalRead(btnres)) {
+    bool pres_cont = false;
+    while (!digitalRead(btndn) && currentPosition > 0) {
+      Serial.println(F("Moving down"));
+      small_stepper.step(ccw ? -1 : 1);
+      currentPosition = currentPosition - 1;
+      yield();
+      delay(50);
+      pres_cont = true;
+    }
+    while (!digitalRead(btnup) && currentPosition < maxPosition) {
+      Serial.println(F("Moving up"));
+      small_stepper.step(ccw ? 1 : -1);
+      currentPosition = currentPosition + 1;
+      yield();
+      delay(50);
+      pres_cont = true;
+    }
+    if (pres_cont) {
+      int set = (setPos * 100) / maxPosition;
+      int pos = (currentPosition * 100) / maxPosition;
+      webSocket.broadcastTXT("{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
+      sendmsg(outputTopic, "{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
+      Serial.println(F("Stopped. Reached wanted position"));
+      saveItNow = true;
+    }
   }
 
+  if (!(digitalRead(btnres) || digitalRead(btndn) || digitalRead(btnup))) {
+    Serial.println(F("Hold to reset..."));
+    uint32_t restime = millis();
+    while (!(digitalRead(btnres) || digitalRead(btndn) || digitalRead(btnup)))
+      yield(); //Prevent watchdog trigger
 
-  /**
-    Storing positioning data and turns off the power to the coils
-  */
+    if (millis() - restime >= 2500) {
+      stopPowerToCoils();
+      Serial.println(F("Removing configs..."));
+
+      WiFi.disconnect(true);
+      WiFiManager wifiManager;
+      helper.resetsettings(wifiManager);
+
+      Serial.println(F("Reboot"));
+      ESP.wdtFeed();
+      yield();
+      ESP.restart();
+    }
+  }
+
+  //Storing positioning data and turns off the power to the coils
   if (saveItNow) {
     saveConfig();
     saveItNow = false;
@@ -431,47 +481,45 @@ void loop(void)
       consumption
     */
     stopPowerToCoils();
-
   }
 
-  /**
-    Manage actions. Steering of the blind
-  */
+  //Manage actions. Steering of the blind
   if (action == "auto") {
-    /*
-       Automatically open or close blind
-    */
-    if (currentPosition > path){
-      small_stepper.step(ccw ? -1: 1);
+
+    //Automatically open or close blind
+    if (currentPosition > path) {
+      Serial.println(F("Moving down"));
+      small_stepper.step(ccw ? -1 : 1);
       currentPosition = currentPosition - 1;
-    } else if (currentPosition < path){
+    } else if (currentPosition < path) {
+      Serial.println(F("Moving up"));
       small_stepper.step(ccw ? 1 : -1);
       currentPosition = currentPosition + 1;
     } else {
       path = 0;
       action = "";
-      int set = (setPos * 100)/maxPosition;
-      int pos = (currentPosition * 100)/maxPosition;
-      webSocket.broadcastTXT("{ \"set\":"+String(set)+", \"position\":"+String(pos)+" }");
-      sendmsg(outputTopic, "{ \"set\":"+String(set)+", \"position\":"+String(pos)+" }");
-      Serial.println("Stopped. Reached wanted position");
+      int set = (setPos * 100) / maxPosition;
+      int pos = (currentPosition * 100) / maxPosition;
+      webSocket.broadcastTXT("{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
+      sendmsg(outputTopic, "{ \"set\":" + String(set) + ", \"position\":" + String(pos) + " }");
+      Serial.println(F("Stopped. Reached wanted position"));
       saveItNow = true;
     }
 
- } else if (action == "manual" && path != 0) {
-    /*
-       Manually running the blind
-    */
+  } else if (action == "manual" && path != 0) {
+
+    //Manually running the blind
     small_stepper.step(ccw ? path : -path);
     currentPosition = currentPosition + path;
+    Serial.println(F("Moving motor manually"));
   }
 
   /*
-     After running setup() the motor might still have
-     power on some of the coils. This is making sure that
-     power is off the first time loop() has been executed
-     to avoid heating the stepper motor draining
-     unnecessary current
+    After running setup() the motor might still have
+    power on some of the coils. This is making sure that
+    power is off the first time loop() has been executed
+    to avoid heating the stepper motor draining
+    unnecessary current
   */
   if (initLoop) {
     initLoop = false;
